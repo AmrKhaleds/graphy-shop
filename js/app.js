12d08b1b2f774af36b9a5c002b1dbd32d5f30120
AmrKhaledS <amrkhaleds898@gmail.com>
2021-05-16 19:49:28 +0200

    Modified app js navbar to be fixed when it's scroll

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-// Js Main File
+// Main Js file
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 70) {
+            document.getElementById('navbar_top').classList.add('sticky');
+        } else {
+            document.getElementById('navbar_top').classList.remove('sticky');
+        } 
+    });
+}); 
